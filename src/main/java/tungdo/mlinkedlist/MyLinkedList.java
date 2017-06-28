8e35b51f93fc3a835a2138f1939c88b4a31f357d
Tung Do <DoThanhTung@Tungs-MacBook-Pro.local>
2017-06-28 11:58:46 +0700

    Add generic type

--- src/main/java/tungdo/mlinkedlist/MyLinkedList.java
+++ src/main/java/tungdo/mlinkedlist/MyLinkedList.java
@@ -1,8 +1,8 @@
 package tungdo.mlinkedlist;
 
-public class MyLinkedList {
-	protected Node start;
-	protected Node end;
+public class MyLinkedList<E extends Comparable<? super E>> {
+	protected Node<E> start;
+	protected Node<E> end;
 	protected int size;
 
 	public MyLinkedList() {
@@ -19,8 +19,8 @@ public class MyLinkedList {
 		return size;
 	}
 
-	public void appendFirst(int val) {
-		Node newNode = new Node(val, null);
+	public void appendFirst(E val) {
+		Node<E> newNode = new Node<E>(val, null);
 		size++;
 		if (start == null) {
 			start = newNode;
@@ -31,8 +31,8 @@ public class MyLinkedList {
 		}
 	}
 
-	public void appendLast(int val) {
-		Node newNode = new Node(val, null);
+	public void appendLast(E val) {
+		Node<E> newNode = new Node<E>(val, null);
 		size++;
 		if (start == null) {
 			start = newNode;
@@ -43,7 +43,7 @@ public class MyLinkedList {
 		}
 	}
 
-	public void appendAt(int val, int pos) {
+	public void appendAt(E val, int pos) {
 
 		if (pos == 0) {
 			appendFirst(val);
@@ -55,11 +55,11 @@ public class MyLinkedList {
 			return;
 		}
 
-		Node newNode = new Node(val, null);
-		Node currentNode = start;
+		Node<E> newNode = new Node<E>(val, null);
+		Node<E> currentNode = start;
 		for (int i = 1; i < size; i++) {
 			if (i == pos) {
-				Node tmpNode = currentNode.nextNode;
+				Node<E> tmpNode = currentNode.nextNode;
 				currentNode.nextNode = newNode;
 				newNode.nextNode = tmpNode;
 				size++;
@@ -69,11 +69,11 @@ public class MyLinkedList {
 		}
 	}
 
-	public void removeAll(int val) {
-		Node currentNode = start;
+	public void removeAll(E val) {
+		Node<E> currentNode = start;
 		int position = 0;
 		while (position < size) {
-			if (currentNode.data > val) {
+			if (currentNode.data.compareTo(val) > 0) {
 				removeAt(position);
 				position--;
 			}
@@ -96,8 +96,8 @@ public class MyLinkedList {
 		}
 
 		if (pos == size - 1) {
-			Node current = start;
-			Node tmp = start;
+			Node<E> current = start;
+			Node<E> tmp = start;
 			while (current != end) {
 				tmp = current;
 				current = current.nextNode;
@@ -108,10 +108,10 @@ public class MyLinkedList {
 			return;
 		}
 
-		Node currentNode = start;
+		Node<E> currentNode = start;
 		for (int i = 1; i < size - 1; i++) {
 			if (i == pos) {
-				Node tmp = currentNode.nextNode.nextNode;
+				Node<E> tmp = currentNode.nextNode.nextNode;
 				currentNode.nextNode = tmp;
 				break;
 			}
@@ -126,7 +126,7 @@ public class MyLinkedList {
 			return "[]";
 		StringBuilder sb = new StringBuilder();
 		String prefix = "";
-		Node currentNode = start;
+		Node<E> currentNode = start;
 		int count = 0;
 		while (count != size) {
 			sb.append(prefix);
@@ -138,11 +138,11 @@ public class MyLinkedList {
 		return sb.toString();
 	}
 
-	class Node {
-		protected int data;
-		protected Node nextNode;
+	class Node<T> {
+		protected T data;
+		protected Node<T> nextNode;
 
-		public Node(int data, Node nextNode) {
+		public Node(T data, Node<T> nextNode) {
 			this.data = data;
 			this.nextNode = nextNode;
 		}
--- src/test/java/tungdo/mlinkedlist/MyLinkedListTest.java
+++ src/test/java/tungdo/mlinkedlist/MyLinkedListTest.java
@@ -5,17 +5,36 @@ import org.junit.Test;
 public class MyLinkedListTest {
 	@Test
 	public void test(){
-		MyLinkedList list = new MyLinkedList();
-		list.appendFirst(10);
-		list.appendFirst(20);
-		list.appendFirst(40);
-		list.appendFirst(30);
-		list.appendLast(50);
-		list.appendLast(15);
-		System.out.println(list);
-		list.removeAt(5);
-		System.out.println(list);
-		list.removeAll(35);
-		System.out.println(list);
+		MyLinkedList<Integer> integerList = new MyLinkedList<>();
+		integerList.appendFirst(10);
+		integerList.appendFirst(20);
+		integerList.appendFirst(40);
+		integerList.appendFirst(30);
+		integerList.appendLast(50);
+		integerList.appendLast(15);
+		System.out.println("List integer: ");
+		System.out.println(integerList);
+		integerList.removeAt(5);
+		System.out.println("Remove element at index 5:");
+		System.out.println(integerList);
+		integerList.removeAll(35);
+		System.out.println("Remove element greater than 35:");
+		System.out.println(integerList);
+		//
+		MyLinkedList<String> stringList = new MyLinkedList<>();
+		stringList.appendFirst("A");
+		stringList.appendFirst("B");
+		stringList.appendFirst("C");
+		stringList.appendFirst("D");
+		stringList.appendLast("E");
+		stringList.appendLast("F");
+		System.out.println("List string: ");
+		System.out.println(stringList);
+		stringList.removeAt(5);
+		System.out.println("Remove element at index 5:");
+		System.out.println(stringList);
+		stringList.removeAll("C");
+		System.out.println("Remove element greater than C");
+		System.out.println(stringList);
 	}
 }
